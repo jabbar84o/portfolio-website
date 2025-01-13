b14ad3b93aec30791c678d782e8a9a97707f77bf
CodewithAbdulJabbar <167693370+jabbar84o@users.noreply.github.com>
2025-01-13 17:47:11 +0500

    Add files via upload

---
+++ Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='mt-16 py-6 text-center bg-accent'>
+      @ Abdul Jabbar | All rights reserved 2025.
+    </div>
+  )
+}
+
+export default Footer
